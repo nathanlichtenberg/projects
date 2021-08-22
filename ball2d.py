edbdee4e2cc10e2852720bc6cfce71256d5e9a76
nathanlichtenberg <lichtenbergnathan@gmail.com>
2021-08-22 08:56:54 +0800

    ball2d

--- ball2d.py
+++ ball2d.py
@@ -1,12 +1,16 @@
 import pygame
+import random
 
-W = 600
+W = 1000
 H = 800
 
 radius = 50
 
-y = H - 2 * radius
-up = True
+x = 10
+y = 10
+
+x_vel = random.randint(5,15)
+y_vel = random.randint(6,16)
 
 pygame.init()
 screen = pygame.display.set_mode((W,H))
@@ -19,22 +23,19 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    if y <= 0:
-        up = False
-    if y >= H - 2 * radius:
-        up = True
+    if y <= 0 or y >= H - 2 * radius:
+        y_vel = -1 * y_vel
 
-    if up:
-        y -= 6
-    else:
-        y += 6
+    if x <= 0 or x >= W - 2 * radius:
+        x_vel = -1 * x_vel
 
-    
+
+    x += x_vel
+    y += y_vel
     screen.fill((255,255,255))
-    pygame.draw.ellipse(screen, (0,0,0), (W/2 - radius, y, 2 * radius, 2 * radius))
+    pygame.draw.ellipse(screen, (0,0,0), (x, y, 2 * radius, 2 * radius))
     pygame.display.update()
     clock.tick(60)
-
     
 
 
