e09e7b7b7a1da32aab7f96319a5fc707cdd1a709
nathanlichtenberg <83146214+nathanlichtenberg@users.noreply.github.com>
2021-10-17 09:02:40 +0800

    Add files via upload

--- Drawing.py
+++ Drawing.py
@@ -4,7 +4,7 @@ from pygame.math import Vector2
 
 W = 1000
 H = 800
-#               Gray                     Red           Yellow           Blue
+#          Gray          Red       Yellow      Blue
 colors = [(140,140,140),(255,0,0),(255,255,0),(0,0,255)]
 choice = 0
 
@@ -20,6 +20,12 @@ class Ball:
         self.pos = pygame.mouse.get_pos()
         pygame.draw.ellipse(screen, self.color, (self.pos[0]-self.radius, self.pos[1]-self.radius, 2 * self.radius, 2 * self.radius))
 
+class Point:
+
+    def __init__(self,pos,color):
+        self.pos = pos
+        self.color = color
+
     
 pygame.init()
 screen = pygame.display.set_mode((W,H))
@@ -33,23 +39,23 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                click_pos.append(pygame.mouse.get_pos())
         if pygame.mouse.get_pressed()[0]:
-            click_pos.append(pygame.mouse.get_pos())
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_c:
-                click_pos = []
-            if event.key == pygame.K_s:
+            point = Point(pygame.mouse.get_pos(),colors[choice])
+            click_pos.append(point)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 3:
                 choice += 1
                 if choice == len(colors):
                     choice = 0
                 ball.color = colors[choice]
-        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                click_pos = []
     screen.fill((255,255,255))
-    for pos in click_pos:
-         pygame.draw.ellipse(screen, colors[choice], (pos[0]-ball.radius, pos[1]-ball.radius, 2 * ball.radius, 2 * ball.radius))
+    for point in click_pos:
+        color = point.color
+        pos = point.pos
+        pygame.draw.ellipse(screen, color, (pos[0]-ball.radius, pos[1]-ball.radius, 2 * ball.radius, 2 * ball.radius))
     ball.move()
     pygame.display.update()
     clock.tick(100)
