566201f0f0c64da625968213e0cd49f9bac4cd9c
nathanlichtenberg <lichtenbergnathan@gmail.com>
2021-10-24 09:01:35 +0800

    added buttons

--- Drawing.py
+++ Drawing.py
@@ -26,7 +26,15 @@ class Point:
         self.pos = pos
         self.color = color
 
-    
+class Button:
+
+    def __init__(self,x,y,width,height,screen,color):
+        self.rect = pygame.Rect(x,y,width,height)
+        self.screen = screen
+        self.color = color
+        
+    def paint(self):
+        pygame.draw.rect(self.screen,self.color,self.rect)
 pygame.init()
 screen = pygame.display.set_mode((W,H))
 pygame.display.set_caption("cursorchaser")
@@ -34,6 +42,11 @@ clock = pygame.time.Clock()
 
 ball = Ball(Vector2(10,10),15,colors[choice])
 click_pos = []
+button_gray = Button(W-50,0,50,50,screen,colors[0])
+button_red = Button(W-50,50,50,50,screen,colors[1])
+button_yellow = Button(W-50,100,50,50,screen,colors[2])
+button_blue = Button(W-50,150,50,50,screen,colors[3])
+buttons = [button_gray, button_red, button_yellow, button_blue]
 
 while True:
     for event in pygame.event.get():
@@ -43,11 +56,13 @@ while True:
             point = Point(pygame.mouse.get_pos(),colors[choice])
             click_pos.append(point)
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 3:
-                choice += 1
-                if choice == len(colors):
-                    choice = 0
-                ball.color = colors[choice]
+            if event.button == 1:
+                for button in buttons:
+                    if button.rect.collidepoint(event.pos):
+                        choice += 1
+                        if choice == len(colors):
+                            choice = 0
+                        ball.color = colors[choice]
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_c:
                 click_pos = []
@@ -56,6 +71,8 @@ while True:
         color = point.color
         pos = point.pos
         pygame.draw.ellipse(screen, color, (pos[0]-ball.radius, pos[1]-ball.radius, 2 * ball.radius, 2 * ball.radius))
+    for button in buttons:
+        button.paint()
     ball.move()
     pygame.display.update()
     clock.tick(100)
