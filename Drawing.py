7db6c53890dcbaf0e01c60ff2ea1719def64a2ee
nathanlichtenberg <lichtenbergnathan@gmail.com>
2021-10-31 08:59:48 +0800

    Buttons fixed and clear

--- Drawing.py
+++ Drawing.py
@@ -6,7 +6,6 @@ W = 1000
 H = 800
 #          Gray          Red       Yellow      Blue
 colors = [(140,140,140),(255,0,0),(255,255,0),(0,0,255)]
-choice = 0
 
 class Ball:
 
@@ -40,7 +39,7 @@ screen = pygame.display.set_mode((W,H))
 pygame.display.set_caption("cursorchaser")
 clock = pygame.time.Clock()
 
-ball = Ball(Vector2(10,10),15,colors[choice])
+ball = Ball(Vector2(10,10),15,colors[0])
 click_pos = []
 button_gray = Button(W-50,0,50,50,screen,colors[0])
 button_red = Button(W-50,50,50,50,screen,colors[1])
@@ -48,24 +47,24 @@ button_yellow = Button(W-50,100,50,50,screen,colors[2])
 button_blue = Button(W-50,150,50,50,screen,colors[3])
 buttons = [button_gray, button_red, button_yellow, button_blue]
 
+trash_icon = pygame.image.load("trash_icon.jpeg").convert_alpha()
+trash_icon = pygame.transform.scale(trash_icon,(80,80))
+trash_rect = trash_icon.get_rect(bottomright = (W,H))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         if pygame.mouse.get_pressed()[0]:
-            point = Point(pygame.mouse.get_pos(),colors[choice])
+            point = Point(pygame.mouse.get_pos(),ball.color)
             click_pos.append(point)
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 for button in buttons:
                     if button.rect.collidepoint(event.pos):
-                        choice += 1
-                        if choice == len(colors):
-                            choice = 0
-                        ball.color = colors[choice]
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_c:
-                click_pos = []
+                        ball.color = button.color
+                if trash_rect.collidepoint(event.pos):
+                    click_pos = []
     screen.fill((255,255,255))
     for point in click_pos:
         color = point.color
@@ -73,6 +72,7 @@ while True:
         pygame.draw.ellipse(screen, color, (pos[0]-ball.radius, pos[1]-ball.radius, 2 * ball.radius, 2 * ball.radius))
     for button in buttons:
         button.paint()
+    screen.blit(trash_icon,trash_rect)
     ball.move()
     pygame.display.update()
     clock.tick(100)
