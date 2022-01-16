0b63d2fa6e5eb7b8d6ff2f42d79f156aea37d856
nathanlichtenberg <lichtenbergnathan@gmail.com>
2022-01-16 10:00:50 +0800

    added spawn ball

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+import time
 from pygame.math import Vector2
 
 W = 1000
@@ -8,26 +9,30 @@ H = 800
 
 class Ball:
 
-    def __init__(self,position,width,color):
+    def __init__(self,width,color):
         self.color = color
-        self.rect = pygame.Rect(position[0], position[1], width, width)
-        self.spawn_ball()
+        self.width = width
+        self.rect = pygame.Rect(W//2, H//2, self.width, self.width)
+        self.direction = Vector2(0,0)
+        self.show = False
+        
     def move(self,screen):
         if self.rect.y <= 0 or self.rect.y >= H - 2 * self.rect.width:
             self.direction[1] = -1 * self.direction[1]
 
         self.rect.move_ip(self.direction[0], self.direction[1])
-        
-        pygame.draw.rect(screen, self.color, self.rect)
+
+        if self.show:
+            pygame.draw.rect(screen, self.color, self.rect)
 
 
 
     def spawn_ball(self):
-        center = Vector2(W/2,H/2)
+        self.show = True
+        self.rect = pygame.Rect(W//2, H//2, self.width, self.width)
         angle = math.radians(random.choice([0,90,180,270]) + random.randint(20,60))
         speed = random.randint(8,12)
         self.direction = Vector2(math.cos(angle),-1*math.sin(angle))
-        
         self.direction.scale_to_length(speed)
 
 class Paddle:
@@ -54,7 +59,8 @@ class Game:
         self.clock = pygame.time.Clock()
         self.left_paddle = Paddle((35,H//2),20,130)
         self.right_paddle = Paddle((W-35,H//2),20,130)
-        self.ball = Ball((W//2, H//2),25,(255,255,255))
+        self.ball = Ball(25,(255,255,255))
+        self.start = False
 
     def draw_net(self):
         pygame.draw.rect(self.screen,(255,255,255),(W//2,25,25,25))
@@ -74,34 +80,42 @@ class Game:
         pygame.draw.rect(self.screen,(255,255,255),(W//2,725,25,25))
         pygame.draw.rect(self.screen,(255,255,255),(W//2,775,25,25))
 
+
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1 and self.start == False:
+                        time.sleep(0.5)
+                        self.start = True
+                        self.ball.spawn_ball()
+
                 
             self.screen.fill((0,0,0))
             self.draw_net()
 
+            if self.start:
+                keys = pygame.key.get_pressed()
+                
+                if keys[pygame.K_UP]:
+                    self.right_paddle.move(-10)
+                elif keys[pygame.K_DOWN]:
+                    self.right_paddle.move(10)
+                if keys[pygame.K_w]:
+                    self.left_paddle.move(-10)
+                elif keys[pygame.K_s]:
+                    self.left_paddle.move(10)
 
-            keys = pygame.key.get_pressed()
-            
-            if keys[pygame.K_UP]:
-                self.right_paddle.move(-10)
-            elif keys[pygame.K_DOWN]:
-                self.right_paddle.move(10)
-            if keys[pygame.K_w]:
-                self.left_paddle.move(-10)
-            elif keys[pygame.K_s]:
-                self.left_paddle.move(10)
 
+                if self.ball.rect.colliderect(self.left_paddle):
+                    if self.ball.direction[0] < 0:
+                        self.ball.direction[0] *= -1
 
-
-            if self.ball.rect.colliderect(self.left_paddle):
-                self.ball.direction[0] *= -1
-
-            if self.ball.rect.colliderect(self.right_paddle):
-                self.ball.direction[0] *= -1
+                if self.ball.rect.colliderect(self.right_paddle):
+                    if self.ball.direction[0] > 0:
+                        self.ball.direction[0] *= -1
                 
             self.left_paddle.draw(self.screen)
             self.right_paddle.draw(self.screen)
