0f2c64a10092f119bab38746f7a262e483a8dff1
nathanlichtenberg <83146214+nathanlichtenberg@users.noreply.github.com>
2022-01-16 09:39:18 +0800

    Add files via upload

--- pong.py
+++ pong.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 import math
-import time
 from pygame.math import Vector2
 
 W = 1000
@@ -9,10 +8,12 @@ H = 800
 
 class Ball:
 
-    def __init__(self,position,width,color):
+    def __init__(self,width,color):
         self.color = color
-        self.rect = pygame.Rect(position[0], position[1], width, width)
-        self.spawn_ball()
+        self.width = width
+        self.rect = pygame.Rect(W//2, H//2, self.width, self.width)
+        self.direction = Vector2(0,0)
+        
     def move(self,screen):
         if self.rect.y <= 0 or self.rect.y >= H - 2 * self.rect.width:
             self.direction[1] = -1 * self.direction[1]
@@ -24,11 +25,10 @@ class Ball:
 
 
     def spawn_ball(self):
-        center = Vector2(W/2,H/2)
+        self.rect = pygame.Rect(W//2, H//2, self.width, self.width)
         angle = math.radians(random.choice([0,90,180,270]) + random.randint(20,60))
         speed = random.randint(8,12)
         self.direction = Vector2(math.cos(angle),-1*math.sin(angle))
-        
         self.direction.scale_to_length(speed)
 
 class Paddle:
@@ -55,43 +55,46 @@ class Game:
         self.clock = pygame.time.Clock()
         self.left_paddle = Paddle((35,H//2),20,130)
         self.right_paddle = Paddle((W-35,H//2),20,130)
-        self.ball = Ball((W//2, H//2),25,(255,255,255))
+        self.ball = Ball(25,(255,255,255))
 
     def draw_net(self):
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,25,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,75,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,125,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,175,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,225,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,275,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,325,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,375,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,425,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,475,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,525,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,575,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,625,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,675,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,725,25,25))
-        pygame.draw.rect(self.screen,(255,255,255),(W//2-12.5,775,25,25))
-
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,25,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,75,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,125,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,175,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,225,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,275,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,325,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,375,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,425,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,475,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,525,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,575,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,625,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,675,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,725,25,25))
+        pygame.draw.rect(self.screen,(255,255,255),(W//2,775,25,25))
 
     def draw_menu(self):
         pygame.draw.rect(self.screen,(64,0,128),(W//2-250,H-300,500,100), border_radius = 20)
         font = pygame.font.SysFont("Arial", 60, True, False)
         text = font.render('Start Game', True, (77,255,255))
         self.screen.blit(text,(200,200,200,200))
-        
+
+
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.ball.spawn_ball()
                 
             self.screen.fill((0,0,0))
             self.draw_net()
             self.draw_menu()
-            
+
 
             keys = pygame.key.get_pressed()
             
@@ -103,7 +106,6 @@ class Game:
                 self.left_paddle.move(-10)
             elif keys[pygame.K_s]:
                 self.left_paddle.move(10)
-
 
 
             if self.ball.rect.colliderect(self.left_paddle):
