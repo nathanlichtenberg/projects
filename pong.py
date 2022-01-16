68b6144d3359c55d48b1ece665f3e5d5caf1d297
nathanlichtenberg <83146214+nathanlichtenberg@users.noreply.github.com>
2022-01-16 10:02:04 +0800

    Add files via upload

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+import time
 from pygame.math import Vector2
 
 W = 1000
@@ -13,18 +14,21 @@ class Ball:
         self.width = width
         self.rect = pygame.Rect(W//2, H//2, self.width, self.width)
         self.direction = Vector2(0,0)
+        self.show = False
         
     def move(self,screen):
         if self.rect.y <= 0 or self.rect.y >= H - 2 * self.rect.width:
             self.direction[1] = -1 * self.direction[1]
 
         self.rect.move_ip(self.direction[0], self.direction[1])
-        
-        pygame.draw.rect(screen, self.color, self.rect)
+
+        if self.show:
+            pygame.draw.rect(screen, self.color, self.rect)
 
 
 
     def spawn_ball(self):
+        self.show = True
         self.rect = pygame.Rect(W//2, H//2, self.width, self.width)
         angle = math.radians(random.choice([0,90,180,270]) + random.randint(20,60))
         speed = random.randint(8,12)
@@ -56,6 +60,7 @@ class Game:
         self.left_paddle = Paddle((35,H//2),20,130)
         self.right_paddle = Paddle((W-35,H//2),20,130)
         self.ball = Ball(25,(255,255,255))
+        self.start = False
 
     def draw_net(self):
         pygame.draw.rect(self.screen,(255,255,255),(W//2,25,25,25))
@@ -75,12 +80,6 @@ class Game:
         pygame.draw.rect(self.screen,(255,255,255),(W//2,725,25,25))
         pygame.draw.rect(self.screen,(255,255,255),(W//2,775,25,25))
 
-    def draw_menu(self):
-        pygame.draw.rect(self.screen,(64,0,128),(W//2-250,H-300,500,100), border_radius = 20)
-        font = pygame.font.SysFont("Arial", 60, True, False)
-        text = font.render('Start Game', True, (77,255,255))
-        self.screen.blit(text,(200,200,200,200))
-
 
     def run(self):
         while True:
@@ -88,33 +87,35 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
+                    if event.button == 1 and self.start == False:
+                        time.sleep(0.5)
+                        self.start = True
                         self.ball.spawn_ball()
+
                 
             self.screen.fill((0,0,0))
             self.draw_net()
-            self.draw_menu()
+
+            if self.start:
+                keys = pygame.key.get_pressed()
+                
+                if keys[pygame.K_UP]:
+                    self.right_paddle.move(-10)
+                elif keys[pygame.K_DOWN]:
+                    self.right_paddle.move(10)
+                if keys[pygame.K_w]:
+                    self.left_paddle.move(-10)
+                elif keys[pygame.K_s]:
+                    self.left_paddle.move(10)
 
 
-            keys = pygame.key.get_pressed()
-            
-            if keys[pygame.K_UP]:
-                self.right_paddle.move(-10)
-            elif keys[pygame.K_DOWN]:
-                self.right_paddle.move(10)
-            if keys[pygame.K_w]:
-                self.left_paddle.move(-10)
-            elif keys[pygame.K_s]:
-                self.left_paddle.move(10)
+                if self.ball.rect.colliderect(self.left_paddle):
+                    if self.ball.direction[0] < 0:
+                        self.ball.direction[0] *= -1
 
-
-            if self.ball.rect.colliderect(self.left_paddle):
-                if self.ball.direction[0] < 0:
-                    self.ball.direction[0] *= -1
-
-            if self.ball.rect.colliderect(self.right_paddle):
-                if self.ball.direction[0] > 0:
-                    self.ball.direction[0] *= -1
+                if self.ball.rect.colliderect(self.right_paddle):
+                    if self.ball.direction[0] > 0:
+                        self.ball.direction[0] *= -1
                 
             self.left_paddle.draw(self.screen)
             self.right_paddle.draw(self.screen)
