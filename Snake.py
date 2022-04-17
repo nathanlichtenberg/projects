b949ed789143d9ab9bdd6cc1f0afd475eda3cb86
nathanlichtenberg <lichtenbergnathan@gmail.com>
2022-04-17 08:56:45 +0800

    snake proper movement

--- Snake.py
+++ Snake.py
@@ -11,25 +11,31 @@ BLOCK_COUNT = W//SQUARE_WIDTH
 
 class Snake:
     def __init__(self):
-        self.pos = Vector2(9,9)
-        self.direction = Vector2(0,0)
+        self.reset()
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (77,153,0), (SQUARE_WIDTH * self.pos[0], SQUARE_WIDTH * self.pos[1], SQUARE_WIDTH, SQUARE_WIDTH))
+        for block in self.snake_pos:
+            pygame.draw.rect(screen, (77,153,0), (SQUARE_WIDTH * block[0], SQUARE_WIDTH * block[1], SQUARE_WIDTH, SQUARE_WIDTH))
 
     def move(self):
-        self.pos += self.direction
+        self.snake_pos = self.snake_pos[1:]
+        last_block = self.snake_pos[-1]
+        self.snake_pos.append(last_block + self.direction)
 
+    def reset(self):
+        self.snake_pos = [Vector2(9,9), Vector2(9,9), Vector2(9,9)]
+        self.direction = Vector2(0,0)
+        
 class Fruit:
     def __init__(self):
-        self.pos = Vector2(18, 18)
+        self.pos = Vector2(18,18)
+        
 
     def draw(self, screen):
         pygame.draw.rect(screen, (255,0,43), (SQUARE_WIDTH * self.pos[0], SQUARE_WIDTH * self.pos[1], SQUARE_WIDTH, SQUARE_WIDTH))
 
 class Grid:
     pass
-
 
 class Game:
     def __init__(self):
@@ -39,7 +45,7 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.SCREEN_UPDATE = pygame.USEREVENT
-        pygame.time.set_timer(self.SCREEN_UPDATE, 10)
+        pygame.time.set_timer(self.SCREEN_UPDATE, 100)
 
         self.fruit = Fruit()
         self.snake = Snake()
@@ -52,13 +58,13 @@ class Game:
                 if event.type == self.SCREEN_UPDATE:
                     self.snake.move()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_UP and self.snake.direction != Vector2(0,1):
                         self.snake.direction = Vector2(0,-1)
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_DOWN and self.snake.direction != Vector2(0,-1):
                         self.snake.direction = Vector2(0,1)
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_LEFT and self.snake.direction != Vector2(1,0):
                         self.snake.direction = Vector2(-1,0)
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_RIGHT and self.snake.direction != Vector2(-1,0):
                         self.snake.direction = Vector2(1,0)
 
             self.screen.fill((0,0,0))
